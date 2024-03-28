0585cb872b5c90ba8c91c8c3a353dcd014ce8b20
Francesco Gentile <francescogentilekr@gmail.com>
2024-03-28 17:18:06 +0100

    add style

--- src/components/DetailComponent.tsx
+++ src/components/DetailComponent.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import SingleArticle from "../interfaces/SingleArticle";
+import { useParams, Link } from "react-router-dom";
 
 const DetailComponent = () => {
   const [singleArticle, setSingleArticle] = useState<SingleArticle | null>(null);
+  const param = useParams();
 
   const fetchSingleArticle = async () => {
     try {
-      const response = await fetch("https://api.spaceflightnewsapi.net/v4/articles/:id");
+      const response = await fetch("https://api.spaceflightnewsapi.net/v4/articles/" + param.id);
       if (response.ok) {
         const singleResponse = await response.json();
         console.log(singleResponse);
@@ -23,16 +25,15 @@ const DetailComponent = () => {
 
   return (
     <Container>
-      <Row>
-        <Col>
+      <Row className="justify-content-center mt-3">
+        <Col xs={8}>
           <Card>
             <Card.Img variant="top" src={singleArticle?.image_url} />
             <Card.Body>
-              <Card.Title>{singleArticle?.title}</Card.Title>{" "}
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <Card.Title>{singleArticle?.title}</Card.Title> <Card.Text>{singleArticle?.summary}</Card.Text>
+              <Link to="/">
+                <Button>HOME</Button>
+              </Link>
             </Card.Body>
           </Card>
         </Col>
